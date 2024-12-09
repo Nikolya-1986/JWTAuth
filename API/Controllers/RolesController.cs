9baa707bbb15f341f087f3ba6f8c2b4d0975425c
Nikolai Amosov <nikolai_sergeevich@mail.ru>
2024-12-09 17:18:28 +0300

    RoleAssignDto

--- API/Controllers/RolesController.cs
+++ API/Controllers/RolesController.cs
@@ -60,5 +60,56 @@ namespace API.Controllers
 
             return Ok(roles);
         }
+
+        [HttpDelete("remove/{id}")]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            // find role by their id
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role is null)
+            {
+                return NotFound("Role not found.");
+            }
+
+            var result = await _roleManager.DeleteAsync(role);
+
+            if (result.Succeeded)
+            {
+                return Ok( new { message="Role deleted successfully." });
+            }
+
+            return BadRequest("Role deletion failed.");
+        }
+
+        [HttpPost("assign")]
+        public async Task<IActionResult> AssignRole([FromBody] RoleAssignDto roleAssignDto)
+        {
+            var user = await _userManager.FindByIdAsync(roleAssignDto.UserId);
+
+            if (user is null)
+            {
+                return NotFound("User not found.");
+            }
+
+            var role = await _roleManager.FindByIdAsync(roleAssignDto.RoleId);
+
+            if (role is null)
+            {
+                return NotFound("Role not found.");
+            }
+
+            var result = await _userManager.AddToRoleAsync(user,role.Name!);
+
+            if (result.Succeeded)
+            {
+                return Ok(new { message="Role assigned successfully" });
+            }
+
+            var error = result.Errors.FirstOrDefault();
+
+            return BadRequest(error!.Description);
+
+        }
     }
 }
